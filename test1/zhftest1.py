fa35a9522dc4ac1b6eb360940628d569c2414d16
z15175303556@163.com <z15175303556@163.com>
2024-05-24 06:43:15 +0000

    test6

--- test1/zhftest1.py
+++ test1/zhftest1.py
@@ -1,8 +1,7 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC --建表
+# MAGIC ----建表
 # MAGIC create table test(id int, name string);
 # MAGIC --插入数据
 # MAGIC insert into table test valuse(1,'zhftest')
-# MAGIC
 # MAGIC
