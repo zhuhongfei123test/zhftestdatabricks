46f41b87b144f3c752ef3b17f0907ce54a63c0a2
z15175303556@163.com <z15175303556@163.com>
2024-05-24 07:19:19 +0000

    test1111

--- test1/zhftest1.py
+++ test1/zhftest1.py
@@ -4,9 +4,11 @@
 # MAGIC create table test(id int, name string);
 # MAGIC --插入数据
 # MAGIC insert into table test valuse(1,'zhftest')
+# MAGIC --xiemengfeitest
+# MAGIC --zhftest
 # MAGIC
-# MAGIC ----建表
+# MAGIC --建表
 # MAGIC create table test2(id int, name string);
-# MAGIC --插入数据
+# MAGIC
 # MAGIC insert into table test2 valuse(1,'zhftest')
 # MAGIC
